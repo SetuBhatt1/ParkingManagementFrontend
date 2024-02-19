5275dcfc38bf16cfacac4f77e9be8019ebffa73b
Setu Bhatt <bansetu1@gmail.com>
2024-02-19 09:48:48 +0530

    created a basic project structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Floors from './pages/Floors';
 
 function App() {
   return (
     <>
-     <h1>Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/floors' element={<Floors />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/Floors.jsx
@@ -0,0 +1,7 @@
+const Floors = () => {
+    return(
+        <h1>Floors Page</h1>
+    )
+}
+
+export default Floors;
