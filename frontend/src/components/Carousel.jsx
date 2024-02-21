d0e7e5ebb570bde783539becb2803f5d796e62d5
Setu Bhatt <bansetu1@gmail.com>
2024-02-21 20:00:57 +0530

    removed unnecessary code from carousel component, yet final styling of carousel is left

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -2,18 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { MDBCarousel, MDBCarouselItem } from 'mdb-react-ui-kit';
 
 export default function Carousel() {
-    const [carouselHeight, setCarouselHeight] = useState('100vh');
-
-    useEffect(() => {
-        // Assuming navbar height is  60px
-        const navbarHeight = 60; // replace with your navbar's actual height
-        const viewportHeight = window.innerHeight;
-        const newHeight = viewportHeight - navbarHeight;
-        setCarouselHeight(newHeight + 'px');
-    }, []);
-
     return (
-        <div style={{ position: "absolute", left: 0, top: "10vh", width: "70vw", overflow: "hidden", height: carouselHeight }}>
+        <div style={{ position: "absolute", left: 0, top: "10vh", width: "70vw", overflow: "hidden" }}>
             <MDBCarousel showIndicators interval={3000} style={{ width: "100%", height: "100%" }}>
                 <MDBCarouselItem itemId={1}>
                     <img src='https://mdbootstrap.com/img/new/slides/041.jpg' className='d-block w-100' alt='...' style={{ height: "90vh" }} />
